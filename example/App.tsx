3a0f2761e89020cfb5c30d5e59c9c41e64cd29f5
Anton Yarmak <perfect583@gmail.com>
2024-10-03 15:20:24 +0300

    feat: random word

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-import * as GetDailyWord from 'get-daily-word';
+import * as GetDailyWord from "get-daily-word";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{GetDailyWord.hello()}</Text>
+      <Text>Daily word: {GetDailyWord.getDailyWord()}</Text>
     </View>
   );
 }
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
